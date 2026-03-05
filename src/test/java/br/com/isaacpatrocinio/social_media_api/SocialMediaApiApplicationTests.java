00236454ec1829a61d9190452ba0e142eb6e96ff
= <patrociniodev@gmail.com>
2026-03-05 19:22:06 -0300

    Initialize project

---
+++ src/test/java/br/com/isaacpatrocinio/social_media_api/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaacpatrocinio.social_media_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
