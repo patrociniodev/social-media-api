242a580ee6c1ea1fef11fb8ba65f976c7cdfacd1
Isaac Patrocinio <patrociniodev@gmail.com>
2026-03-12 17:33:39 -0300

    Add User deletion

--- src/main/java/br/com/isaacpatrocinio/social_media_api/resources/UserResource.java
+++ src/main/java/br/com/isaacpatrocinio/social_media_api/resources/UserResource.java
@@ -44,4 +44,10 @@ public class UserResource {
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/isaacpatrocinio/social_media_api/services/UserService.java
+++ src/main/java/br/com/isaacpatrocinio/social_media_api/services/UserService.java
@@ -34,4 +34,9 @@ public class UserService {
     public User fromDTO(UserDTO objDTO) {
         return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
+
+    public void delete(String id) {
+        User obj = findById(id);
+        db.delete(obj);
+    }
 }
